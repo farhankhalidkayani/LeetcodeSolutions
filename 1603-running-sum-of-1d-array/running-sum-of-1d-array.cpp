ed34dc3d0812385d59904996ba1c035fb3cf3955
Farhan Khalid <151544521+farhankhalidkayani@users.noreply.github.com>
2024-05-21 18:32:31 +0500

    Update running-sum-of-1d-array.cpp
    Removed unused iterator

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        vector<int>::iterator itr;
+      
         vector<int> result;
         int ans=0;
         for(int i=0;i<nums.size();i++){
