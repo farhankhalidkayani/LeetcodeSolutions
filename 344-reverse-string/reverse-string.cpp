0992682c30cafaab99ddab824aa288c6efe81fd7
Farhan Khalid <151544521+farhankhalidkayani@users.noreply.github.com>
2024-05-22 18:12:28 +0500

    Time: 14 ms (82.12%) | Memory: 27 MB (60.48%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -4,9 +4,7 @@ public:
        int n= s.size();
        
        for(int i=0;i<n/2;i++){
-        int temp= s[i];
-        s[i]=s[n-1-i];
-        s[n-1-i]=temp;
+        swap(s[i],s[n-i-1]);
        }
       
        
