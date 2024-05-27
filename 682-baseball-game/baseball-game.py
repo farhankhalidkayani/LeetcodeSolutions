b90e12dea3e467d12e3918fc0553ec6b60f21028
Farhan Khalid <151544521+farhankhalidkayani@users.noreply.github.com>
2024-05-27 14:54:43 +0500

    Time: 26 ms (28.87%) | Memory: 12 MB (70.80%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -1,21 +1,16 @@
 class Solution(object):
     def calPoints(self, operations):
         stack=[]
-        result=0
+    
         for op in operations:
             if op=="C":
                 stack.pop()
             elif op=="D":
                 stack.append(stack[-1]*2)
             elif op=="+":
-                a=stack[-1]
-                stack.pop()
-                b=stack[-1]
-                stack.append(a)
-                stack.append(a+b)                    
+                
+                stack.append(stack[-1]+stack[-2])                    
             else:
                 stack.append(int(op))
-        for i in range(len(stack)):
-            result+=stack[-1]
-            stack.pop()
-        return result
+        
+        return sum(stack)
