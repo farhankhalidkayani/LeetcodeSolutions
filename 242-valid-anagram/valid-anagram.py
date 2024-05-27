5b18bd86cd590e6a5bf9201197885ddef774054d
Farhan Khalid <151544521+farhankhalidkayani@users.noreply.github.com>
2024-05-27 13:31:28 +0500

    Time: 38 ms (37.76%) | Memory: 13.5 MB (16.50%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,5 +1,18 @@
 class Solution(object):
     def isAnagram(self, s, t):
       
-        return Counter(s)==Counter(t)         
+     
         
+        if len(s)!=len(t):
+            return False
+        hashtables={}
+        hashtablet={}
+
+        for i in range(len(s)):
+            hashtables[s[i]]=1+hashtables.get(s[i],0)
+            hashtablet[t[i]]=1+hashtablet.get(t[i],0)
+        for val in s:
+            if hashtables[val]!=hashtablet.get(val,0):
+                return False;        
+                        
+        return True
