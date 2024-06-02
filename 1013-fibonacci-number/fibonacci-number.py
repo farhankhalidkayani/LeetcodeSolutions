b4081f01383bb505f376d87e08877fcd17661b61
Farhan Khalid <151544521+farhankhalidkayani@users.noreply.github.com>
2024-06-02 13:11:41 +0500

    Time: 38 ms (55.17%) | Memory: 16.5 MB (24.06%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,8 +1,11 @@
-class Solution(object):
-    def fib(self, n):
-        if n == 1:
-            return 1
-        if n==0:
-            return 0
-        return self.fib(n-1)+self.fib(n-2)
+class Solution:
+    def fib(self, n: int) -> int:
+        memorize={0:0,1:1}
+        def f(x):
+            if x in memorize:
+                return memorize[x]
+            else:
+                memorize[x]=f(x-1)+f(x-2)
+                return memorize[x]
+        return f(n)
         
