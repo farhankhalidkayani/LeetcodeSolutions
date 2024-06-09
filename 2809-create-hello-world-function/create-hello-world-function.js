b9ac9c33585f3cff1152ddad7a2ccc0fc4c47886
Farhan Khalid <151544521+farhankhalidkayani@users.noreply.github.com>
2024-06-09 12:48:22 +0500

    Time: 53 ms (39.25%) | Memory: 48.3 MB (87.46%) - LeetSync

--- 2809-create-hello-world-function/create-hello-world-function.js
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -1,14 +1,11 @@
 /**
  * @return {Function}
  */
-var createHelloWorld = function() {
-    
-    return function(...args) {
-        return ("Hello World")
-        
-    }
+const createHelloWorld=()=>{
+    return (...args)=>{
+        return "Hello World";
+    };
 };
-
 /**
  * const f = createHelloWorld();
  * f(); // "Hello World"
