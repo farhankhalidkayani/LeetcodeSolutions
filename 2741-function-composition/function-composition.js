291ee70f99c8fc4b96eddb936f49315a47294861
Farhan Khalid <151544521+farhankhalidkayani@users.noreply.github.com>
2024-06-10 12:12:30 +0500

    Time: 67 ms (38.46%) | Memory: 50 MB (66.22%) - LeetSync

--- 2741-function-composition/function-composition.js
+++ 2741-function-composition/function-composition.js
@@ -6,11 +6,7 @@ var compose = function(functions) {
     
     return function(x) {
         
-        for(let i=functions.length-1;i>=0;i--){
-            x=functions[i](x);
-            
-        }
-        return x
+        return functions.reduceRight((acc,f)=>{return f(acc)},x);
             
         }
     };
