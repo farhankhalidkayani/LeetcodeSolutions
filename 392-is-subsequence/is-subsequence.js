28cd74a66055443b562bde4dfa1ff88a323cf04b
Farhan Khalid <151544521+farhankhalidkayani@users.noreply.github.com>
2024-06-15 17:08:48 +0500

    Time: 69 ms (5.04%) | Memory: 48.7 MB (86.18%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let l=0;
+    let r=0;
+    
+    while(l<s.length && r<t.length){
+        if(s[l]==t[r]){
+            l++;
+            r++;
+        }
+        else{
+            r++;
+        }
+    }
+    if(l==s.length){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+};
