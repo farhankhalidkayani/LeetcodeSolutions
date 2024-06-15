274249ce957038aebeaf7c2b79b0bafe93a8f45e
Farhan Khalid <151544521+farhankhalidkayani@users.noreply.github.com>
2024-06-15 12:14:47 +0500

    Time: 65 ms (17.43%) | Memory: 50.7 MB (55.53%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.js
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.js
@@ -3,9 +3,11 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    let sum=nums;
-    for(let i=1;i<nums.length;i++){
-        sum[i]+=sum[i-1];
+    let sum=[];
+    let total=0
+    for(let i=0;i<nums.length;i++){
+        total+=nums[i];
+        sum.push(total);
     }
     return sum;
 };
