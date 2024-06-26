5eeca6976641a6eec79b026f0ebb586ca1878f67
Farhan Khalid <151544521+farhankhalidkayani@users.noreply.github.com>
2024-06-26 11:07:16 +0500

    Time: 75 ms (62.37%) | Memory: 54.2 MB (51.51%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -3,14 +3,13 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let NonZeroIndex=0;
+    let Nonzero=0;
     for(let i=0;i<nums.length;i++){
-    if(nums[i]!=0){
-        nums[NonZeroIndex]=nums[i];
-        NonZeroIndex++;
+        if(nums[i]!=0){
+            nums[Nonzero++]=nums[i];
+        }
     }
-    }
-    for(let i=NonZeroIndex;i<nums.length;i++){
-    nums[i]=0;
+    while(Nonzero<nums.length){
+        nums[Nonzero++]=0;
     }
 };
