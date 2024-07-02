3650262d3269e8148f86fe262c4e25c86cdf28e1
Farhan Khalid <151544521+farhankhalidkayani@users.noreply.github.com>
2024-07-02 10:32:10 +0500

    Time: 49 ms (90.90%) | Memory: 51 MB (58.78%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
@@ -4,16 +4,8 @@
  * @return {number[]}
  */
 var intersect = function(nums1, nums2) {
-    let hashtable1=new Map();
     let hashtable2=new Map();
-    for(let c of nums1){
-        if(hashtable1.has(c)){
-            hashtable1.set(c,hashtable1.get(c)+1);
-        }
-        else{
-            hashtable1.set(c,1);
-        }
-    }
+    
     for(let c of nums2){
         if(hashtable2.has(c)){
             hashtable2.set(c,hashtable2.get(c)+1);
