d25620f6dfbc9c92492d80aad55e1fc5d00fc931
Farhan Khalid <151544521+farhankhalidkayani@users.noreply.github.com>
2024-07-14 20:24:10 +0500

    Time: 121 ms (20.63%) | Memory: 19.2 MB (13.33%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        count=Counter(nums)
+        for num in nums:
+            if count[num]==1:
+                return num
+        
