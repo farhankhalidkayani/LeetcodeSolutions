995fc45bfd4021c63bdaa8d781f30b0bfcb4484e
Farhan Khalid <151544521+farhankhalidkayani@users.noreply.github.com>
2024-12-24 16:36:01 +0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (6.16%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.py
+++ 225-implement-stack-using-queues/implement-stack-using-queues.py
@@ -1,26 +1,30 @@
+from collections import deque
 class MyStack:
 
     def __init__(self):
-        self.stack=[]
+        self.queue1=deque()
+        self.queue2=deque()
         
 
     def push(self, x: int) -> None:
-        self.stack.append(x)
-        
+        self.queue1.append(x)
 
     def pop(self) -> int:
-        return self.stack.pop()
+        for i in range(len(self.queue1)-1):
+            x=self.queue1.popleft()
+            self.queue2.append(x)
+        for i in range(len(self.queue2)):
+            x=self.queue2.popleft()
+            self.queue1.append(x)
+        return self.queue1.popleft()
         
 
     def top(self) -> int:
-        return self.stack[-1]
+        return self.queue1[-1]
         
 
     def empty(self) -> bool:
-        if len(self.stack)==0:
-            return True
-        else:
-            return False
+        return len(self.queue1)==0
         
 
 
